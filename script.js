abf0b0c6d602f3e23ebbe0cfa981994ddb00a1f0
Gediminas <115144042+Gecrossas@users.noreply.github.com>
2023-10-21 15:51:51 +0300

    Add class DisplayHandler to handle all rendering of html elements

--- script.js
+++ script.js
@@ -61,8 +61,8 @@ class Dialog {
             this.#dispose();
             // FIXME: Send event instead of using global functions inside a class
             Library.addBookToLibrary(returnedBook);
-            ClearBookCards();
-            generateBookCards();
+            DisplayHandler.clearBookCards();
+            DisplayHandler.generateBookCards();
         }
     }
 
@@ -104,90 +104,96 @@ class Library {
     }
 }
 
-const addButton = document.querySelector(".add-book");
-const cardsParent = document.querySelector(".book-cards");
+class DisplayHandler {
+    static #addButton = document.querySelector(".add-book");
+    static #cardsParent = document.querySelector(".book-cards");
+
+    static init = () => {
+        this.generateBookCards();
+        this.#addButton.addEventListener("click", () => {
+            new Dialog();
+        });
+        
+        this.#cardsParent.addEventListener("click", (event) => {
+            if (event.target.classList.contains("remove-book")) {
+                //FIXME: add event instead of external reference to Library
+                Library.removeBookFromLibrary(event.target.getAttribute("data-index"));
+                this.clearBookCards();
+                this.generateBookCards();
+            }
+        });
+    }
+
+    static clearBookCards() {
+        const cards = document.querySelectorAll(".book-card");
+        cards.forEach(card => {
+            card.remove();
+        })
+    }
+
+    static generateBookCards() {
+        //FIXME: add event instead of external reference to Library
+        Library.books.forEach(book => {
+            this.#addNewBookCard(book.title, book.author, book.numberOfPages, book.haveRead, Library.books.indexOf(book));
+        })
+    }
+    
+    static #addNewBookCard(title, author, pages, haveRead, index) {
+        // Create a new div element with the "book-card" class
+        const bookCard = document.createElement("div");
+        bookCard.className = "book-card";
+    
+        // Create an h3 element for the title
+        const titleElement = document.createElement("h3");
+        titleElement.textContent = title;
+    
+        // Create a p element for the author
+        const authorElement = document.createElement("p");
+        authorElement.textContent = author;
+    
+        // Create a p element for the number of pages
+        const pagesElement = document.createElement("p");
+        pagesElement.textContent = `Pages: ${pages}`;
+    
+        // Create a div element for the read checkbox
+        const readDiv = document.createElement("div");
+    
+        // Create a label element for the checkbox
+        const readLabel = document.createElement("label");
+        readLabel.setAttribute("for", "haveRead");
+        readLabel.textContent = "Read";
+    
+        // Create the input element (checkbox)
+        const readInput = document.createElement("input");
+        readInput.setAttribute("type", "checkbox");
+        readInput.setAttribute("name", "haveRead");
+        readInput.setAttribute("id", "haveRead");
+        readInput.checked = haveRead; // Set the checkbox's checked state
+        readInput.disabled = true;
+    
+        // Create button for removing book from library
+        const removeButton = document.createElement("button");
+        removeButton.className = "remove-book";
+        removeButton.textContent = "Remove";
+        removeButton.setAttribute("data-index", index);
+    
+        // Append the elements to the bookCard
+        bookCard.appendChild(titleElement);
+        bookCard.appendChild(authorElement);
+        bookCard.appendChild(pagesElement);
+        readDiv.appendChild(readLabel);
+        readDiv.appendChild(readInput);
+        bookCard.appendChild(readDiv);
+        bookCard.appendChild(removeButton);
+    
+        // Append the bookCard to the .book-cards container
+        const bookContainer = document.querySelector(".book-cards");
+        bookContainer.appendChild(bookCard);
+    }
+}
 
 Library.addBookToLibrary(new Book("The Hobbit", "J.R.R. Tolkien", 295, true));
 Library.addBookToLibrary(new Book("Dune", "Some Genius", 317, true));
 Library.addBookToLibrary(new Book("50 Shades of Gray", "Some Idiot", 69, false));
-generateBookCards();
 
-addButton.addEventListener("click", () => {
-    new Dialog();
-});
-
-cardsParent.addEventListener("click", (event) => {
-    if (event.target.classList.contains("remove-book")) {
-        Library.removeBookFromLibrary(event.target.getAttribute("data-index"));
-        ClearBookCards();
-        generateBookCards();
-    }
-});
-
-
-//TODO: Crate a class containing below functions
-function ClearBookCards() {
-    const cards = document.querySelectorAll(".book-card");
-    cards.forEach(card => {
-        card.remove();
-    })
-}
-
-function generateBookCards() {
-    Library.books.forEach(book => {
-        addNewBookCard(book.title, book.author, book.numberOfPages, book.haveRead, Library.books.indexOf(book));
-    })
-}
-
-function addNewBookCard(title, author, pages, haveRead, index) {
-    // Create a new div element with the "book-card" class
-    const bookCard = document.createElement("div");
-    bookCard.className = "book-card";
-
-    // Create an h3 element for the title
-    const titleElement = document.createElement("h3");
-    titleElement.textContent = title;
-
-    // Create a p element for the author
-    const authorElement = document.createElement("p");
-    authorElement.textContent = author;
-
-    // Create a p element for the number of pages
-    const pagesElement = document.createElement("p");
-    pagesElement.textContent = `Pages: ${pages}`;
-
-    // Create a div element for the read checkbox
-    const readDiv = document.createElement("div");
-
-    // Create a label element for the checkbox
-    const readLabel = document.createElement("label");
-    readLabel.setAttribute("for", "haveRead");
-    readLabel.textContent = "Read";
-
-    // Create the input element (checkbox)
-    const readInput = document.createElement("input");
-    readInput.setAttribute("type", "checkbox");
-    readInput.setAttribute("name", "haveRead");
-    readInput.setAttribute("id", "haveRead");
-    readInput.checked = haveRead; // Set the checkbox's checked state
-    readInput.disabled = true;
-
-    // Create button for removing book from library
-    const removeButton = document.createElement("button");
-    removeButton.className = "remove-book";
-    removeButton.textContent = "Remove";
-    removeButton.setAttribute("data-index", index);
-
-    // Append the elements to the bookCard
-    bookCard.appendChild(titleElement);
-    bookCard.appendChild(authorElement);
-    bookCard.appendChild(pagesElement);
-    readDiv.appendChild(readLabel);
-    readDiv.appendChild(readInput);
-    bookCard.appendChild(readDiv);
-    bookCard.appendChild(removeButton);
-
-    // Append the bookCard to the .book-cards container
-    const bookContainer = document.querySelector(".book-cards");
-    bookContainer.appendChild(bookCard);
-}
+DisplayHandler.init();
